114fb61e741c0983fc4c48f763e9ab9f0a1ea7fe
Alejandro PG <ing.alejandro99@gmail.com>
2022-11-24 00:02:01 -0600

    se agrego api

--- src/app/menu/usuario/usuario.page.ts
+++ src/app/menu/usuario/usuario.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-usuario',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuario.page.scss'],
 })
 export class UsuarioPage implements OnInit {
-
-  constructor() { }
+  caracteres = [];
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
+    this.http.get<any>('https://myparkingtesithon.000webhostapp.com/json.php')
+    .subscribe(res => {
+      console.log(res);
+      this.caracteres = res.results;
+    })
+    
   }
 
 }
